a82070bb09013ff9971e34cb7532cc3c00efee3e
NomanSadick <nomansadickjs@gmail.com>
2022-06-28 16:27:43 +0600

    done

--- js/app.js
+++ js/app.js
@@ -34,4 +34,21 @@ document.getElementById('key-pad').addEventListener('click',function(event){
     }
 
 })
+
+function veriFyPin(){
+    const pin = document.getElementById('display-pin').value;
+    const typeNumbers = document.getElementById('type-numbers').value;
+    const success = document.getElementById('notify-success');
+    const error = document.getElementById('notify-fail');
+    if (pin == typeNumbers) {
+        success.style.display = 'block';
+        error.style.display = 'none';
+    }
+    else{
+        success.style.display = 'none';
+        error.style.display = 'block';
+        
+    }
+
+}
     
